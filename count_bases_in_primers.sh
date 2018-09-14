a4fdc9342c5641b7419b3aed9ca63243598675a4
Trishel Cruzado <tpcruzado@dons.usfca.edu>
2018-09-14 15:08:54 -0700

    Generalize script to take command line option

--- count_bases_in_primers.sh
+++ count_bases_in_primers.sh
@@ -17,4 +17,4 @@
 # sigh.
 # Another solution would be to use tr like so
 # head -2 primer_B.fasta | tail -1 | tr -d '\n' | wc -m
-expr $(head -n 2 primer_A.fasta | tail -1 | wc -m) - 1
+expr $(head -n 2 "$@" | tail -1 | wc -m) - 1
