8135c86f4be28536a71b89f9ba7b3c8d801aad37
Trishel Cruzado <tpcruzado@dons.usfca.edu>
2018-09-14 15:22:01 -0700

    Add in for loop to handle multiple files

--- count_bases_in_primers.sh
+++ count_bases_in_primers.sh
@@ -17,4 +17,11 @@
 # sigh.
 # Another solution would be to use tr like so
 # head -2 primer_B.fasta | tail -1 | tr -d '\n' | wc -m
-expr $(head -n 2 "$@" | tail -1 | wc -m) - 1
+# what we found in our google search in class:
+# expr $(head -n 2 "$@" | tail -1 | wc -m) - 1
+for file in "$@"
+do
+FILENAME=$(basename $file .fasta)
+COUNT=$(expr $(head -n 2 $file | tail -1 | wc -m) - 1)
+  echo In $FILENAME, there are $COUNT nucleotides  
+done
